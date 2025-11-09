8750ca51c02933758885d8e2d4824ada1081237e
marcelkb <marcelkb@pm.me>
2025-11-09 15:54:56 +0100

    typo

--- reya_ccxt_adapter/sdk_patch.py
+++ reya_ccxt_adapter/sdk_patch.py
@@ -2,7 +2,7 @@ import sys
 import types
 
 def patch_sdk_version():
-    # patch sdk since pyprojext.toml file is not beeing imported if sdk is used as a lib in version 2.0.6.1
+    # patch sdk since pyproject.toml file is not beeing imported if sdk is used as a lib in version 2.0.6.1
     mock_version_module = types.ModuleType('sdk._version')
     mock_version_module.SDK_VERSION = "2.0.6.1"
     sys.modules['sdk._version'] = mock_version_module
